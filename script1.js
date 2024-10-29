1788cd78d2f241a5cc95e826ebb83fed6dcaa1d8
Matthew <matthew41305@gmail.com>
2024-10-28 18:05:14 -0700

    finished creating # triangle separated it into two different loops to create the upper and lower parts of triangle

--- script1.js
+++ script1.js
@@ -7,3 +7,25 @@
 // ###
 // ##
 // #
+
+const height = 4;
+
+// loop for upper part of triangle
+for (let i=1; i<= height; i++) {
+    let line = ''; // empty line
+    for (let j=1; j <= i; j++) {
+        line += '#' // what it will print each line
+    }
+    console.log(line); // current line displayed
+}
+
+
+// loop for lower section of the triangle
+
+for (let i= height - 1; i>= 1; i--){
+    let line = ''; // empty line again
+    for (let j =1; j <= i; j++) {
+        line += '#' // prints # each line
+    }
+    console.log(line); // shows current line
+}
