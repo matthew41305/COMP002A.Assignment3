40a6ad788cf69b65da8f51a96c791d9f31d3782d
Matthew <matthew41305@gmail.com>
2024-10-30 13:50:46 -0700

    fixed script 3, now alternating with odd

--- script3.js
+++ script3.js
@@ -17,12 +17,12 @@ const size = 8;
 for (let row = 0; row < size; row++) {
     let line = '';
 
-    for (let col = 0; col <size; col++) {
+    for (let col = 0; col < size; col++) {
+        // Check if the sum of row + col is even or odd
         if ((row + col) % 2 == 0) {
             line += '#';
-        }else {
-            line += '';
-
+        } else {
+            line += ' ';  // Add a space for alternating pattern
         }
     }
     console.log(line);
